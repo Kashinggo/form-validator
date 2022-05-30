093f70c063d219866e5c3bdfceafc211ecc10930
kscode <kashinggo@gmail.com>
2022-05-30 23:29:10 +0800

    check required & refactor

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const password2 = document.getElementById("password2");
 // Show input error message
 function showError(input, message) {
   const formControl = input.parentElement;
-  formControl.classList.add("error");
+  // formControl.classList.add("error");
+  formControl.className = "form-control error";
   const small = formControl.querySelector("small");
   small.innerText = message;
 }
@@ -29,29 +30,25 @@ function isValidEmail(email) {
     );
 }
 
+// Check required fields
+function checkRequired(inputArr) {
+  inputArr.forEach((input) => {
+    if (input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+// Get field name
+function getFieldName(input) {
+  // Attention: id instead of value
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
 // Event listener
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  if (username.value === "") {
-    showError(username, "Username is required");
-  } else {
-    showSuccess(username);
-  }
-  if (email.value === "") {
-    showError(email, "Email is required");
-  } else if (!isValidEmail(email.value)) {
-    showError(email, "Email is not valid");
-  } else {
-    showSuccess(email);
-  }
-  if (password.value === "") {
-    showError(password, "Password is required");
-  } else {
-    showSuccess(password);
-  }
-  if (password2.value === "") {
-    showError(password2, "Password2 is required");
-  } else {
-    showSuccess(password2);
-  }
+  checkRequired([username, email, password, password2]);
 });
